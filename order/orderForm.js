eec7094e4b912bc1953ae95b5c97c5164d829d13
AbdulNishar05 <80945971+AbdulNishar05@users.noreply.github.com>
2021-05-19 21:08:58 +0530

    Update orderForm.js

--- order/orderForm.js
+++ order/orderForm.js
@@ -43,12 +43,17 @@ $(document).ready(function () {
         $("#dup-id1").val('');
     })
 
-    $("#char-id1").keypress(function (event) {
-        var keyCode = event.which;
-        if ((keyCode < 65 || keyCode > 90) && (keyCode < 97 || keyCode > 123) && keyCode != 32) {
-            return false;
-        }
-        return true;
+    $(function () {
+        $(document).on('keydown', ".char", function (event) {
+            if (event.shiftKey || event.ctrlKey || event.altKey) {
+                event.preventDefault();
+            } else {
+                var key = event.keyCode;
+                if (!((key == 8) || (key == 32) || (key == 46) || (key >= 35 && key <= 40) || (key >= 65 && key <= 90))) {
+                    event.preventDefault();
+                }
+            }
+        });
     });
 
     $("#submit").click(function () {
@@ -63,7 +68,7 @@ $(document).ready(function () {
         var dup = [];
         for (let i = 1, j = 0; i <= rows.length; i++, j++) {
             words[j] = $('#char-id' + i).val();
-            large[j] = $('#large-id' + i).val();
+            large[j] = parseInt($('#large-id' + i).val());
             fib[j] = $('#fib-id' + i).val();
             dup[j] = $("#dup-id" + i).val();
             if (words[j] == "" || large[j] == "" || fib[j] == "" || dup[j] == "") {
@@ -71,78 +76,86 @@ $(document).ready(function () {
                 return false
             }
         }
-            function largest(large) {
-                let first = -1, second = -1;
-                for (let i = 0; i < large.length; i++) {
-                    if (large[i] > first) {
-                        second = first;
-                        first = large[i];
-                    }
-                    else if (large[i] > second && large[i] != first) {
-                        second = large[i];
-                    }
-                }
-                $("#large").text(second);
-                console.log(second);
-                return;
-            }
-            largest(large)
+        function largest(large) {
+            var temp, size;
+            size = large.length;
+            for (var i = 0; i < size; i++) {
+                for (var j = i + 1; j < size; j++) {
 
-            function duplicate(dup) {
-                var result = [];
-                for (var i = 0; i < dup.length - 1; i++) {
-                    for (var j = i + 1; j < dup.length; j++) {
-                        if (dup[i] == dup[j]) {
-                            result.push(dup[i]);
-                        }
-                    }
-                }
-                $("#dup").text(result);
-                console.log(result);
-                return;
-            }
-
-            duplicate(dup)
-
-            function Repeated() {
-                for (let i = 0; i < words.length; i++) {
-                    let data = words[i];
-                    for (let j = 0; j < words.length; j++) {
-                        if (i == j) { continue; }
-                        if (data == words[j]) {
-                            $("#words").text(data);
-                            return;
-                        }
+                    if (large[i] > large[j]) {
+                        temp = large[i];
+                        large[i] = large[j];
+                        large[j] = temp;
                     }
                 }
             }
-            Repeated(words);
+            $("#large").text(large[size - 2]);
+            return;
+        }
+        largest(large)
 
-            function fibanocci(fib) {
-                var num1, num2, res, temp = "";
-                for (let i = 0; i < fib.length; i++) {
-                    num1 = 0;
-                    num2 = 1;
-                    res = 0;
-                    res = num1 + num2;
-                    for (let j = 1; j <= fib[i]; j++) {
-                        if (res == fib[i]) {
-                            break;
-                        }
-                        num1 = num2;
-                        num2 = res;
-                        res = num1 + num2;
+        function duplicate(dup) {
+            var count;
+            var result = "";
+            for (let i = 0; i < dup.length - 1; i++) {
+                count = 1;
+                for (let j = i + 1; j < dup.length; j++) {
+                    if (dup[i] == dup[j]) {
+                        dup[j] = '0';
+                        count = 0;
                     }
+                }
+                if (count == 1) {
+                    dup[i] = '0';
+                }
+            }
+            dup[dup.length - 1] = '0';
+            for (let i = 0; i < dup.length; i++) {
+                if (dup[i] != '0')
+                    result = result + " " + dup[i];
+            }
+            $("#dup").text(result);
+            return;
+        }
+        duplicate(dup);
+
+        function Repeated(words) {
+            let obj = new Set();
+            for (let i = 0; i <= words.length - 1; i++) {
+                let char = words[i];
+                if (obj.has(char))
+                    return char;
+                else
+                    obj.add(char);
+            }
+        }
+        $("#words").text(Repeated(words));
+
+        function fibanocci(fib) {
+            var num1, num2, res, temp = "";
+            for (let i = 0; i < fib.length; i++) {
+                num1 = 0;
+                num2 = 1;
+                res = 0;
+                res = num1 + num2;
+                for (let j = 1; j <= fib[i]; j++) {
                     if (res == fib[i]) {
-                        temp = temp + "  " + res;
+                        break;
                     }
+                    num1 = num2;
+                    num2 = res;
+                    res = num1 + num2;
                 }
-                $("#fib").text(temp);
-                console.log(temp);
-                return;
+                if (res == fib[i]) {
+                    temp = temp + "  " + res;
+                }
             }
-            fibanocci(fib);
-        
+            $("#fib").text(temp);
+            console.log(temp);
+            return;
+        }
+        fibanocci(fib);
+
         table.css('display', 'block')
     })
 
